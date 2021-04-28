c16e621329dca856dc54272c14e20437a878acdd
2001979 <e2001979@shell.vamk.fi>
2021-04-28 20:42:08 +0300

    last version

--- score.h
+++ score.h
@@ -4,6 +4,7 @@
 #define MAX 30
 //coment
 // this coment added with github editor
+// this coment added with nano on shell.puv.fi, after pulling github 
 void input_scores(int[]);
 void evaluation(int[], int[]);
 void classification(int[], int[]);
